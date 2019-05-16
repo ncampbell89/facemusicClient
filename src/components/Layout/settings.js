de641f9f3bdcc9d59289643b538b58a0ba81cb69
Nicole Campbell <nscampbell303@gmail.com>
2019-05-16 16:00:24 -0400

    modified

--- src/components/Layout/settings.js
+++ src/components/Layout/settings.js
@@ -2,4 +2,4 @@ export const clientId = '5445014665f14870bc86cc9e7e7a21d6'
 
 export const redirectUri = 'https://facemusic.herokuapp.com'
 
-//export const redirectUri = 'http://localhost:3000'
+// export const redirectUri = 'http://localhost:3000'
--- src/redux/actions/Axios/Axios.js
+++ src/redux/actions/Axios/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: 'https://facemusicserver.herokuapp.com:3001',
+    baseURL: 'https://facemusicserver.herokuapp.com',
     timeout: 3600000 // 1 hour
 });
 
