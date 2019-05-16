4976af8bbf1eccd10750ed45c7c99f11d6851bd6
Nicole Campbell <nscampbell303@gmail.com>
2019-05-16 15:04:42 -0400

    changed

--- src/redux/reducers/genresReducer.js
+++ src/redux/reducers/genresReducer.js
@@ -67,7 +67,6 @@ export default (state = initialState, action) => {
 
 
         case SPOTIFY:
-            console.log(action)
             updated.spotifyGenres = action.payload;
             return updated;
 
