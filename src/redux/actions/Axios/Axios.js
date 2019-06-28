92f11d72ce6c936e60c6302ca8bd23d1a27c7910
Nicole <nscampbell303@gmail.com>
2019-06-28 15:35:03 -0400

    modified

--- src/redux/actions/Axios/Axios.js
+++ src/redux/actions/Axios/Axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: 'https://practical-fermat-c9cbd0.netlify.com',
-    // baseURL: 'https://facespotifymusic.com',
+    // baseURL: 'https://practical-fermat-c9cbd0.netlify.com',
+    baseURL: 'http://facespotifymusic.com',
     // baseURL: 'http://localhost:3001',
     timeout: 3600000 // 1 hour
 });
